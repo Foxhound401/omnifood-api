5865a911a884e52390b5b9d8a0f68a333bcc753e
phungphuc <phungthienphuc18@gmail.com>
2019-07-07 19:16:59 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bap websites, today we create a cloud service for noob' });
+  res.render('index', { title: 'Brody websites, today we create a cloud service for noob' });
 });
 
 module.exports = router;
