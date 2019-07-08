c8533af701699ff17965222a11925dc5c1c1c46e
phungphuc <phungthienphuc18@gmail.com>
2019-07-08 17:51:15 +0700

    update git submodule

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //  res.send('respond with a resource');
+  
+  res.json([
+    {
+      id: 1,
+      username: "sample01"
+    },
+    {
+      id: 2,
+      username: "dolores01"
+    }
+  ]);
 });
 
 module.exports = router;
